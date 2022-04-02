b5e767dec52ab2c6092f75807cc1dc323dd271e4
M. Yoga Nugraha <myoganugraha@gmail.com>
2022-04-03 01:12:44 +0800

    create constants for firestore

---
+++ lib/constants/firestore_constants.dart
@@ -0,0 +1,9 @@
+class FirestoreConstants {
+  static const pathUserCollection = 'users';
+  static const pathMessageCollection = "messages";
+  static const nickname = "name";
+  static const email = "email";
+  static const photoUrl = "photoUrl";
+  static const id = "id";
+  static const chattingWith = "chattingWith";
+}
