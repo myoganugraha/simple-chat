27b0497cf223a048aa64c4c94c2bc4043d3e9965
M. Yoga Nugraha <myoganugraha@gmail.com>
2022-04-03 13:46:51 +0800

    add message model class

--- lib/constants/firestore_constants.dart
+++ lib/constants/firestore_constants.dart
@@ -6,4 +6,9 @@ class FirestoreConstants {
   static const photoUrl = "photoUrl";
   static const id = "id";
   static const chattingWith = "chattingWith";
+  static const idFrom = "idFrom";
+  static const idTo = "idTo";
+  static const timestamp = "timestamp";
+  static const content = "content";
+  static const type = "type";
 }
